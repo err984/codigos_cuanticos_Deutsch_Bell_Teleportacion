ca26f30e925b46db923efc3b969e7797dc6cc61d
err984 <113684638+err984@users.noreply.github.com>
2026-05-20 09:46:12 +0200

    Create Deutsch0.py

---
+++ Deutsch/f(x)=0/Deutsch0.py
@@ -0,0 +1,39 @@
+from qiskit import QuantumCircuit, transpile
+
+backend=QmioBackend() # loads the last calibration file from the directory $QMIO_CALIBRARTIONS
+
+# Circuito
+qc = QuantumCircuit(2)
+
+#Preparar el estado inicial (qubit 1 en |1>)
+qc.x(1) 
+
+# Aplicar barrera y puertas Hadamard para crear superposicion
+qc.barrier()  
+qc.h(0)
+qc.h(1)
+
+#El "Oraculo" (Identidad en este caso)
+qc.id(0) 
+
+#Generamos interferencia
+qc.barrier()
+qc.h(0)
+
+#Medimos
+qc.measure_all()
+qc.draw()
+
+#Transpile the circuit using the optimization_level equal to 2
+qct=transpile(qc,backend)
+
+#Execute the circuit with 4096 shots. Must be executed from a node with a QPU.
+job=backend.run(qct,shots=4096)
+
+#Return the results
+print("Job enviado... esperando resultados")
+
+result = job.result()
+
+counts = result.get_counts(qc)
+print("QmioBackend Counts:", counts)
