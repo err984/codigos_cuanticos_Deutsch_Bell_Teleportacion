2e8509a1d86a89fbf8ba66673057b223840fba92
err984 <113684638+err984@users.noreply.github.com>
2026-05-20 09:26:01 +0200

    Update Bell0110.py

--- Bell/Psi+/Bell0110.py
+++ Bell/Psi+/Bell0110.py
@@ -1,10 +1,20 @@
+from qiskit import QuantumCircuit, transpile
+from qmiotools.integrations.qiskitqmio import QmioBackend
+
+
+backend=QmioBackend() # loads the last calibration file from the directory $QMIO_CALIBRARTIONS
+
+# Circuito pequeñito solo para preparar el estado final teorico
+qc = QuantumCircuit(2)
+
 #Creo superposicion en q0
 qc.h(0)
 
 #Estado entrelazado de Bell phi+
 qc.cx(0,1)
 
-q.z(0)
+#Cambiamos el estado de q1 para crear psi+
+qc.x(1)
 
 #Mediciones
 qc.measure_all()
